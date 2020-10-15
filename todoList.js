af7f7073257e7b0bcb67157afdd1067a585d941e
Vladimir Striber <vladimir.striber@cloudtech.company>
2020-10-15 09:45:33 +0200

    Delete todo button addition

--- todoList.js
+++ todoList.js
@@ -2,26 +2,26 @@
 
 let todoList = {
   todos: [],
-  displayTodos: function () {
-    if (this.todos.length === 0) {
-      alert("Todo list is empty");
-    } else {
-      console.log("My todos:");
-      const check = String.fromCodePoint(0x2705);
-      const cross = String.fromCodePoint(0x274C);
-      for (let i = 0; i < this.todos.length; i++) {
-        console.log(`todo #${i + 1}:`, this.todos[i].todoText, this.todos[i].completed ? check : cross);
-
-        // the other way:
-        // if (this.todos[i].completed === true) {
-        //   console.log(`todo #${i + 1}:`, this.todos[i].todoText, check);
-        // } else {
-        //   console.log(`todo #${i + 1}:`, this.todos[i].todoText, cross);
-        // }
-
-      }
-    }
-  },
+  // displayTodos: function () {
+  //   if (this.todos.length === 0) {
+  //     alert("Todo list is empty");
+  //   } else {
+  //     console.log("My todos:");
+  //     const check = String.fromCodePoint(0x2705);
+  //     const cross = String.fromCodePoint(0x274C);
+  //     for (let i = 0; i < this.todos.length; i++) {
+  //       console.log(`todo #${i + 1}:`, this.todos[i].todoText, this.todos[i].completed ? check : cross);
+  //
+  //       // the other way:
+  //       // if (this.todos[i].completed === true) {
+  //       //   console.log(`todo #${i + 1}:`, this.todos[i].todoText, check);
+  //       // } else {
+  //       //   console.log(`todo #${i + 1}:`, this.todos[i].todoText, cross);
+  //       // }
+  //
+  //     }
+  //   }
+  // },
   addTodo: function (todoValue) {
 
     this.todos.push({
@@ -29,8 +29,7 @@ let todoList = {
       completed: false
     });
     // todoValue = "";
-    // console.log(todoValue, "value");
-    this.displayTodos();
+    // this.displayTodos();
   },
   changeTodo: function (index, todoText) {
     // index = document.querySelector(".todoItemIndex").value;
@@ -45,16 +44,16 @@ let todoList = {
     // this.displayTodos();
   },
   toggleCompleted: function (index) {
-    // debugger;
     let todo = this.todos[index];
+
     todo.completed = !todo.completed;
     // this.displayTodos();
   },
   toggleAll: function () {
-    // debugger;
     let completedTodos = [];
     // let completedTodos = 0; // This is different approach
     const totalTodos = this.todos.length;
+
     // check how many items are completed
     for (let i = 0; i < totalTodos; i++) {
       if (this.todos[i].completed) {
@@ -62,71 +61,52 @@ let todoList = {
         // completedTodos++ // This is different approach
       }
     }
-    // console.log(completedTodos, "completed");
     // Scenario 1: if all items are not completed - make all of them completed
     if (completedTodos.length < totalTodos) {
       for (let i = 0; i < totalTodos; i++) {
         this.todos[i].completed = true;
       }
-      // console.log("completed < todos");
     }
     // Scenario 2: if all items are completed - make all of them uncompleted
     if (completedTodos.length === totalTodos) {
       for (let i = 0; i < totalTodos; i++) {
         this.todos[i].completed = false;
       }
-      // console.log("completed = todos");
     }
-    this.displayTodos();
+    // this.displayTodos();
   }
 };
-
-// Old approach
-
-// const displayTodosButton = document.getElementById("displayTodosButton");
-// displayTodosButton.addEventListener("click", function () {
-//   todoList.displayTodos();
-// });
-//
-// const toggleTodosButton = document.getElementById("toggleAllButton");
-// toggleTodosButton.addEventListener("click", function () {
-//   todoList.toggleAll();
-// });
-
-// New approach
 
 let handlers = {
   addTodo: function () {
     let todoValue = document.querySelector(".todoItem");
-    // let todoInput = document.querySelector(".todoItem");
 
     todoList.addTodo(todoValue.value);
     view.displayTodos();
     todoValue.value = "";
   },
-  displayTodos: function () {
-    todoList.displayTodos();
-  },
   toggleAll: function () {
     todoList.toggleAll();
+    view.displayTodos();
   },
   changeTodo: function () {
     let changeTodoIndex = document.querySelector(".todoItemIndex");
     let changeTodoValue = document.querySelector(".changedTodoItemValue");
 
-    // console.log(changeTodoIndex.valueAsNumber, "index value");
-
     todoList.changeTodo(changeTodoIndex.valueAsNumber, changeTodoValue.value);
+    view.displayTodos();
     changeTodoIndex.value = "";
     changeTodoValue.value = "";
   },
   deleteTodo: function () {
     let deleteTodoIndex = document.querySelector(".deletedTodoItemIndex");
+
     todoList.deleteTodo(deleteTodoIndex.valueAsNumber);
     deleteTodoIndex.value = "";
   },
   toggleCompleted: function () {
     let toggleTodoIndex = document.querySelector(".toggledTodoItemIndex");
+
     todoList.toggleCompleted(toggleTodoIndex.valueAsNumber);
     view.displayTodos();
     toggleTodoIndex.value = "";
@@ -135,7 +115,7 @@ let handlers = {
 
 let view = {
   displayTodos: function () {
-    // First approach (not quit what we want)
+    // First approach (not quite what we want)
     // let todoLi = document.createElement("li");
     // let todoUl = document.querySelector(".todoUl");
     // let todoValue = document.querySelector(".todoItem");
@@ -160,16 +140,15 @@ let view = {
 
       todoLi.textContent = todoList.todos[i].todoText;
       todoLi.appendChild(todoCompletedIcon);
+      todoLi.appendChild(this.createDeleteButton());
       todoUl.appendChild(todoLi);
     }
 
   },
+  createDeleteButton: function () {
+    // debugger;
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    return deleteButton;
+  },
 };
-
-// Method calls
-// todoList.displayTodos();
-// todoList.addTodo("First object");
-// todoList.changeTodo(0, "First object modified");
-// todoList.toggleCompleted(0);
-// todoList.deleteTodo();
-
